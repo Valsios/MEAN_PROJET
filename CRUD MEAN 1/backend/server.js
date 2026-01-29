9964df00fa9aef72942bd8dd5b8a2190b55402d2
Valsios <RandriaVals2303@gmail.com>
2026-01-29 21:04:00 +0300

    Initial commit

---
+++ CRUD MEAN 1/backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+require('dotenv').config({ path: "./conf.env" });
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connexion à MongoDB
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB connecté"))
+  .catch(err => console.log(err));    
+
+// Routes
+app.use('/articles', require('./routes/articleRoutes'));
+app.listen(PORT, () => console.log(`Serveur démarré sur le port
+${PORT}`));
